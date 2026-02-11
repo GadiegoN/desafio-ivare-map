de6a2833c162bdb2878049d20fe650b782cfd6df
GadiegoN <ngadiego@gmail.com>
2026-02-11 17:41:53 -0300

    feat: differentiate selected and saved markers with custom icons

--- src/components/map/mapView.tsx
+++ src/components/map/mapView.tsx
@@ -1,7 +1,11 @@
 import type { LatLngExpression } from "leaflet";
+import L from "leaflet";
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import { useUiStore } from "../../store/uiStore";
 import { MapController } from "./mapController";
+import { useQuery } from "@tanstack/react-query";
+import { listPlaces } from "../../lib/placesStorage";
+import type { SavedPlace } from "../../types";
 
 const UBERLANDIA: LatLngExpression = [-18.9146, -48.2754];
 
@@ -17,8 +21,31 @@ function ClickHandler() {
   return null;
 }
 
+const selectedIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
+const savedIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
 export function MapView() {
   const { selectedLat, selectedLng } = useUiStore();
+
+  const placesQuery = useQuery<SavedPlace[]>({
+    queryKey: ["places"],
+    queryFn: async () => listPlaces(),
+  });
 
   return (
     <div className="overflow-hidden rounded-2xl border border-slate-200 bg-white shadow-sm">
@@ -30,8 +57,16 @@ export function MapView() {
           <ClickHandler />
 
           {selectedLat && selectedLng && (
-            <Marker position={[selectedLat, selectedLng]} />
+            <Marker position={[selectedLat, selectedLng]} icon={selectedIcon} />
           )}
+
+          {placesQuery.data?.map((p) => (
+            <Marker
+              key={p.id}
+              position={[p.latitude, p.longitude]}
+              icon={savedIcon}
+            />
+          ))}
         </MapContainer>
       </div>
     </div>
